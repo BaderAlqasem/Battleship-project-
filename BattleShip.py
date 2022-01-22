2ff93503e321bc7cb4d2c24d6afdbb9c90430876
BaderAlqasem <90061531+BaderAlqasem@users.noreply.github.com>
2022-01-21 17:43:57 -0700

    Update BattleShip.py

--- BattleShip.py
+++ BattleShip.py
@@ -1,49 +1,46 @@
 from random import randint
 
+print("Note: row and column start at 0. Top left coordinate is (0, 0)")
 print("You have eight guesses. Good luck.")
 
 board = []
-            
+
 for x in range(0, 5):
-  board.append(["O"] * 5)
-
+    board.append(["O"] * 5)
+    
 def addBoard(board):
-  for row in board:
-    print(" ".join(row))
-
+    for row in board:
+        print(" ".join(row))
+        
 addBoard(board)
 
 def randRow(board):
-  return randint(0, len(board) - 1)
+    return randint(0, len(board) - 1)
 
 def randCol(board):
-  return randint(0, len(board[0]) - 1)
+    return randint(0, len(board [0]) - 1)
 
 shipRow = randRow(board)
 shipCol = randCol(board)
 
-for yourTurn in range(9):
-  print("Turn: ", yourTurn + 1)
-  guessRow = int(input("Guess Row: "))
-  guessCol = int(input("Guess Col: "))
-
-  if guessRow == shipRow and guessCol == shipCol:
-    print("Congratulations! You sank my battleship!")
-    break
-  else:
-    if guessRow not in range(5) or \
-      guessCol not in range(5):
-      print("Oops, that's not even in the ocean.")
-    elif board[guessRow][guessCol] == "X":
-      print( "You guessed that one already." )
-
-    else:
-      print("You missed my battleship!")
-      board[guessRow][guessCol] = "X"
-      if yourTurn == 7:
-        print("Game Over")
-        print("The correct corrdinates were:")
-        print(shipRow)
-        print(shipCol)
+for yourTurn in range (9):
+    print("Turn: ", yourTurn + 1)
+    guessRow = int(input("Guess Row: "))
+    guessCol = int(input("Guess Col: "))
+    if guessRow == shipRow and guessCol == shipCol:
+        print("You Sank my Battleship!")
         break
-    addBoard(board)
+    else:
+        if guessRow not in range(5) or guessCol not in range(5):
+            print("Coordinates inaccessible")
+            
+        elif board[guessRow][guessCol] == "X":
+            print("Coordinates Already Guessed")
+        else:
+            print("Incorrect Guess")
+            board[guessRow][guessCol] = "X"
+        if yourTurn == 7:
+            print("Game over")
+            print("Correct coordinate were: ", shipRow, ",", shipCol)
+            break
+        addBoard(board)
